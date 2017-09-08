95c545cfe85f36f4840d472a8e67349a0bbacb6e
Praveen C <cpraveen@gmail.com>
2017-09-08 09:54:15 +0530

    Adding minimax example

---
+++ approximation/minimax_abs.m
@@ -0,0 +1,12 @@
+close all; clear all
+x = chebfun('x'); f = abs(x);
+for n = 2:2:4
+  figure()
+  subplot(1,2,1), hold off, plot(f,'k','LineWidth',2), grid on
+  [p,err] = remez(f,n); hold on, plot(p,'r','LineWidth',2), axis([-1 1 -.2 1.2])
+  FS = 'fontsize';
+  title(['Function and best approx, n = ' int2str(n)],FS,9)
+  subplot(1,2,2), hold off, plot(f-p,'LineWidth',2), grid on, hold on
+  axis([-1 1 -.15 .15]), title('Error curve',FS,9)
+  plot([-1 1],err*[1 1],'--k'), plot([-1 1],-err*[1 1],'--k')
+end
