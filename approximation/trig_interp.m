5353febfa12587653fd93acd74b7c69799a49ac4
Praveen C <cpraveen@gmail.com>
2017-09-12 14:35:14 +0530

    Updates

--- approximation/trig_interp.m
+++ approximation/trig_interp.m
@@ -1,17 +1,22 @@
+clear all; close all
 
-  fun = @(x) max(0,1-abs(x-pi)/2); 
-  fun = @(x) exp(sin(x));
+% Function to approximate
+fun = @(x) max(0,1-abs(x-pi)/2); 
+fun = @(x) exp(sin(x));
 
-  N = 24; 
-  h = 2*pi/N; 
-  x = h*(0:N-1)';
-  v = fun(x);
+N = 24; 
+h = 2*pi/N; 
+x = h*(0:N-1)';
+v = fun(x);
 
-  v_hat = fft(v);
-  k = [0:N/2, -N/2+1:-1];
+% Compute DFT using FFT
+v_hat = fft(v);
+k = [0:N/2, -N/2+1:-1];
 
-  ng = 100;
-  xg = linspace(0,2*pi,ng);
-  fg = exp(1i*xg'*k) * v_hat / N;
-  fg = real(fg);
-  plot(x,v,'o',xg,fg,'-')
+% Plot on a finer grid
+ng = 100;
+xg = linspace(0,2*pi,ng);
+fg = exp(1i*xg'*k) * v_hat / N;
+fg = real(fg);
+plot(x,v,'o',xg,fg,'-')
+grid on
